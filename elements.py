b0f0db1807ca00d33c6228d08688213fdef66a6a
dmerthe <36462234+dmerthe@users.noreply.github.com>
2020-10-15 00:38:07 -0700

    Added Instrument object

--- elements.py
+++ elements.py
@@ -1,4 +1,69 @@
-import copy
+
+
+class Instrument:
+    """
+    Basic representation of an instrument
+    """
+
+    name = 'Instrument'
+
+    self.knobs = tuple()
+    self.meters = tuple()
+
+    self.knob_values = {}
+
+    def __init__(self, **kwargs):
+
+        self.presets = kwargs.get('presets', [])
+
+        for knob, value in self.presets:
+            self.set(knob, value)
+
+        self.postsets = kwargs.get('postsets', [])
+
+    def set(self, knob, value):
+        """
+        Set the value of a variable associated with this instrument
+
+        :param knob: (string) name of variable to be set
+        :param value: (float/string) value of new variable setting
+        :return: None
+        """
+
+        try:
+            set_method = self.__getattribute__('set_ ' +knob.replace(' ' ,'_'))
+        except AttributeError:
+            raise SetError(f"'{knob}' cannot be set on {self.name}")
+
+        if value is None:  # A None value passed into this function indicates that no change in setting is to be made
+            return
+
+        set_method(value)
+
+    def measure(self, meter):
+        """
+        Measure the value of a variable associated with this instrument
+
+        :param meter: (string) name of the variable to be measured
+        :return: (float/string) measured value of the variable
+        """
+
+        try:
+            measurement_method = self.__getattribute__('measure_ ' +meter.replace(' ' ,'_'))
+        except AttributeError:
+            raise MeasurementError(f"'{meter}' cannot be measured on {self.name}")
+
+        measurement = measurement_method()
+
+        return measurement
+
+    def __del__(self):
+
+        for knob, value in self.postsets.items():
+            self.set(knob, value)
+
+        self.adapter.disconnect()
+
 
 
 class Knob:
@@ -42,10 +107,15 @@ class Meter:
 
 class Dependent:
     """
-    Representation of a experimental variable that is evaluated based on other experiment variables
+    Representation of a experimental variable that depends on other experiment variables
     """
 
     def __init__(self, expression, parents):
+        """
+
+        :param expression: (str) mathematical expression of the variable in terms of the parent variables
+        :param parents: (dict) dictionary of the form {..., variable symbol, knob/meter/dependent object, ....}
+        """
 
         self.expression = expression
         self.parents = parents
@@ -54,10 +124,10 @@ class Dependent:
     @property
     def value(self):
 
-        expression = copy.copy(self.expression)
+        expression = self.expression
 
         for symbol, parent in parents.items():
-            expression.replace(symbol, str(parent._value))
+            expression = expression.replace(symbol, str(parent._value))
 
         self._value = eval(expression)
 
