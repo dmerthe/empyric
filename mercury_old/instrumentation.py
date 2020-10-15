b5a9f1d9d60d3b05053bbefb915d9a4ca69fd070
dmerthe <36462234+dmerthe@users.noreply.github.com>
2020-10-15 00:11:10 -0700

    Start of new mercury build

---
+++ mercury_old/instrumentation.py
@@ -0,0 +1,8 @@
+# This script is a placeholder for all the stuff in the instruments stash, to make for simpler instrument importing
+
+from mercury.instruments.sourcemeters import *
+from mercury.instruments.supplies import *
+from mercury.instruments.multimeters import *
+from mercury.instruments.thermometers import *
+from mercury.instruments.barometers import *
+from mercury.instruments.humans import *
---
+++ mercury_old/instruments/humans.py
@@ -0,0 +1,95 @@
+# A human is just another instrument
+
+from mercury.instruments.basics import *
+
+class ConsoleUser(Instrument):
+
+    name = 'ConsoleUser'
+
+    knobs = ('prompt',  # message or query to send to user
+             'cooldown')  # minimum time before sending repeat messages
+
+    meters = ('response',)
+
+    def __init__(self, address=None, backend=None):
+
+        self.knob_values = {'prompt': '', 'cooldown':60}
+
+        self.last_response = ''
+        self.last_message = ''
+
+    def set_prompt(self, prompt):
+        self.knob_values['prompt'] = prompt
+
+    def set_cooldown(self, cooldown):
+        self.know_values['cooldown'] = cooldown
+
+    def measure_response(self):
+
+        new_message = (self.knob_values['prompt'] != self.last_message)
+
+        if time.time() >= self.last_sent + self.know_values['cooldown'] or new_message:  # don't spam people
+            self.last_response = input(self.knob_values['prompt'] )
+            return self.last_response
+        else:
+            self.last_response
+
+    def disconnect(self):
+        return
+
+class SMSUser(Instrument):
+
+    name = 'SMSUser'
+
+    knobs = ('prompt',  # message or query to send to user
+             'wait time', # time to wait for a user response
+             'cooldown')  # minimum time before sending repeat messages
+
+    meters = ('response',)
+
+    def __init__(self, address, backend='twilio'):
+
+        phone_number = address
+        self.connect(phone_number)
+
+        self.knob_values = {'prompt': '', 'wait time': 5*60, 'cooldown':30*60}
+
+        # Find last received message
+        self.last_sent = -np.inf
+        _, self.last_received = self.read()
+
+    def set_prompt(self, prompt):
+
+        new_message = (self.knob_values['prompt'] != prompt)
+        self.knob_values['prompt'] = prompt
+
+        if time.time() >= self.last_sent + self.know_values['cooldown'] or new_message:  # don't spam people
+            self.write(prompt)
+
+    def set_wait_time(self, wait_time):
+        self.knob_values['wait time'] = wait_time
+
+    def set_cooldown(self, cooldown):
+        self.know_values['cooldown'] = cooldown
+
+    def measure_response(self):
+
+        waiting  = True
+        wait_time = self.knob_values['wait time']
+
+        start_time = time.time()
+
+        while waiting:  # wait for a response
+            body, date_received = self.read()  # get the most recent SMS
+
+            time_difference = date_received - self.last_received
+
+            # Check if a the message is new
+            if time_difference.total_seconds() > 0:
+                waiting = False
+
+            if time.time() - start_time > wait_time:
+                body, date_received = 'NO REPLY', datetime.datetime.fromtimestamp(0)
+
+        self.last_received = date_received
+        return body
