eb2e52998c27e34f3a2f6af0524535e3e3e17c67
dmerthe <36462234+dmerthe@users.noreply.github.com>
2020-10-21 21:07:22 -0700

    Update adapter.py

--- adapter.py
+++ adapter.py
@@ -2,28 +2,31 @@ import importlib
 import functools
 import time
 import warnings
+import sys
 
-supported_adapters = ['dummy', 'serial', 'visa', 'phidget', 'linux']
+supported_adapters = ['serial','gpib', 'usb']
 
 class Adapter:
 
-    failures = 0
-    reconnects = 0
-
-    max_failures = 5
+    max_failures = 3
     max_reconnects = 2
 
     def __init__(self, address, **kwargs):
 
+        # general parameters
         self.address = address
+        self.delay = kwargs.get('delay', 0.1)
+        self.timeout = kwargs.get('timeout', 0.1)
 
-        for key, value in kwargs.items:
-            setattr(self, key, value)
+        # for serial comms
+        self.baud_rate = kwargs.get('baud_rate', 9600)
 
         self.connected = False
+        self.failures = 0
+        self.reconnects = 0
 
     @staticmethod
-    def chaperone(operation):
+    def chaperone(operation):  # wraps all communication methods, deals with communication issues
 
         @functools.wraps(operation)
         def wrapping_function(self, *args, **kwargs):
@@ -41,11 +44,16 @@ class Adapter:
                         if result is not 'invalid':
                             return result
                     except BaseException as err:
-                        warnings.warn(err)
+                        warnings.warn('Encountered '
+                                      + err.__name__
+                                      + f' during communication with {self.type} instrument at address {self.address}')
                         self.failures += 1
                         return wrapping_function(self, *args, **kwargs)
                 else:
-                    self.reconnect()
+                    self.disconnect()
+                    time.sleep(self.delay)
+                    self.connect()
+
                     self.failures = 0
                     return wrapping_function(self, *args, **kwargs)
             else:
@@ -53,108 +61,27 @@ class Adapter:
 
         return wrapping_function
 
-    def reconnect(self):
-        self.disconnect()
-        time.sleep(self.delay)
-        self.connect()
-
-class DummyAdapter(Adapter):
-    """
-    Dummy adapter for testing purposes
-    """
-
-    backend = None
-
-    def __init__(self, address, **kwargs):
-
-        Adapter.__init__()
-
-        self.address = address
-        self.interface = None
-        self.buffer = []
-
-        self.connected = False
-
-    def connect(self):
-        self.connected = True
-
-    def disconnect(self):
-        self.connected = False
-
-    @Adapter.chaperone
-    def write(self, message):
-        self.buffer.append(message)
-
-    @Adapter.chaperone
-    def read(self):
-        return self.buffer.pop(0)
-
     def query(self, message):
         self.write(message)
         return self.read()
 
-class VISAAdapter:
-    """
-    Adapter handling communications with NI-VISA compatible instruments
-    """
-
-    backend = 'visa'
-
-    def __init__(self, address, **kwargs):
-
-        self.address = address
-        self.timeout = kwargs.get('timeout', 0.1)
-        self.delay = kwargs.get('delay', 0)
-
-        if 'ASRL' in address:
-            self.type = 'serial'
-            self.baud_rate = kwargs.get('baud_rate', 9600)
-
-        if 'GPIB' in address:
-            self.type = 'gpib'
-
-        if 'USB' in address:
-            self.type = 'usb'
-
-        self.connected = False
-
+    # Some dummy methods below for testing; should be overwritten by children classes
     def connect(self):
-
-        visa = importlib.import_module('visa')
-        manager = visa.ResourceManager()
-
-        if self.type is 'serial':
-            self.interface = manager.open_resource(self.address, open_timeout=self.timeout, baud_rate=self.baud_rate)
-        else:
-            self.interface = manager.open_resource(self.address, open_timeout=self.timeout)
-
-        self.interface.timeout = self.timeout
-
         self.connected = True
+
+    def disconnect(self):
+        self.connected = False
 
     @Adapter.chaperone
     def write(self, message):
-        self.interface.write(message)
+        self.buffer = 'message'
 
     @Adapter.chaperone
-    def read(self, response_form=None):
-        response = self.interface.read()
-
-        if response is '':
-            return 'invalid'
-        else:
-            return response
-
-    def query(self, question, response_form=None):
-        self.write(question)
-        time.sleep(self.delay)
-        return self.read(response_form=response_form)
-
-    def disconnect(self):
-
-        self.interface.clear()
-        self.interface.close()
-        self.connected = False
+    def read(self):
+        try:
+            return self.buffer
+        except AttributeError:
+            return ''
 
 
 class SerialAdapter(Adapter):
@@ -162,21 +89,12 @@ class SerialAdapter(Adapter):
     Adapter handling communications with serial instruments
     """
 
-    backend = 'visa'
-
-    def __init__(self, address, **kwargs):
-
-        self.address = address
-        self.timeout = kwargs.get('timeout', 0.1)
-        self.delay = kwargs.get('delay', 0)
-        self.baudrate = kwargs.get('baudrate', 9600)
-
-        self.connected = False
+    backend = 'serial'
 
     def connect(self):
 
         serial = importlib.import_module('serial')
-        self.interface = serial.Serial(port=self.address, baudrate=self.baudrate, timeout=self.timeout)
+        self.interface = serial.Serial(port=self.address, baudrate=self.baud_rate, timeout=self.timeout)
 
     @Adapter.chaperone
     def write(self, message):
@@ -204,7 +122,95 @@ class SerialAdapter(Adapter):
         self.connected = False
 
 
-class USBAdapter(Adapter):
+class GPIBAdapter(Adapter):
+    """
+    Representation of a GPIB adapter; implemented using either NI-VISA (Windows/Mac) or the linux module (Linux)
+    """
 
-    def __init__(self):
+    def connect(self):
+
+        if sys.platform in ('win32', 'darwin'):
+            try:
+                self.interface = VISAInterface(self)
+                self.interface.connect()
+                self.backend = 'visa'
+            except ImportError:
+                raise ConnectionError('pyvisa with NI-VISA backend is required for using GPIB adapters with Windows')
+        elif sys.platform is 'linux':
+            try:
+                self.interface = LinuxGPIBInterface(self)
+                self.interface.connect()
+                self.backend = 'linux'
+            except ImportError:
+                raise ConnectionError('linux module is required for using GPIB adapters with Linux')
+        else:
+            raise SystemError(f'{sys.platform} operating system type is not supported!')
+
+
+class Interface:
+
+    def __init__(self, adapter):
+
+        adapter.write = self.write
+        adapter.read = self.read
+
+class VISAAdapter(Adapter):
+    """
+    Adapter handling communications with NI-VISA compatible instruments
+    """
+
+    backend = 'visa'
+
+    def connect(self):
+
+        visa = importlib.import_module('visa')
+        manager = visa.ResourceManager()
+
+        if 'ASRL' in self.address:  # for serial connections
+            self.interface = manager.open_resource(self.address,
+                                                   open_timeout=self.timeout,
+                                                   baud_rate=self.baud_rate,
+                                                   delay=self.delay)
+        else:
+            self.interface = manager.open_resource(self.address,
+                                                   open_timeout=self.timeout,
+                                                   delay=self.delay)
+
+        self.interface.timeout = self.timeout
+
+        self.connected = True
+
+    @Adapter.chaperone
+    def write(self, message):
+        self.interface.write(message)
+
+    @Adapter.chaperone
+    def read(self, response_form=None):
+        response = self.interface.read()
+
+        if response is '':
+            return 'invalid'
+        else:
+            return response
+
+    def query(self, question, response_form=None):
+        self.write(question)
+        return self.read(response_form=response_form)
+
+    def disconnect(self):
+
+        self.interface.clear()
+        self.interface.close()
+        self.connected = False
+
+
+
+
+class LinuxGPIBAdapter(Adapter):
+    """
+    Adapter handling communications with GPIB instruments using the Linux GPIB backend
+    """
+
+    def connect(self):
         pass
+
