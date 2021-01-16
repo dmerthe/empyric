c611283618b76fa61203b5edb9c66b386f733d84
dmerthe <36462234+dmerthe@users.noreply.github.com>
2021-01-16 09:54:58 -0800

    Update scopes.py

--- empyric/collection/scopes.py
+++ empyric/collection/scopes.py
@@ -79,3 +79,36 @@ class TekScope(Instrument):
     @setter
     def set_trigger_level(self, level):
         self.write('TRIG:MAI:LEV %.3e' % level)
+
+    def _measure_channel(self, channel):
+
+        self.write('DAT:ENC ASCI') # ensure ASCII encoding of data
+        self.write('DAT:SOU CH%d' % channel)  # switch to channel 1
+
+        scale_factor = float(self.query('WFMPRE:YMULT?'))
+        zero = float(self.query('WFMPRE:YZERO?'))
+        offset = float(self.query('WFMPRE:YOFF?'))
+
+        self.write('ACQ:STATE RUN') # acquire the waveform
+
+        while int(self.query('BUSY?')):
+            time.sleep(1)  # wait for acquisition to complete
+
+        str_data = self.query('CURVE?').split(' ')[1].split(',')
+        return np.array([ ( float(datum) - offset )*scale_factor + zero for datum in str_data ])
+
+    @measurer
+    def measure_channel_1(self):
+        return self._measure_channel(1)
+
+    @measurer
+    def measure_channel_2(self):
+        return self._measure_channel(2)
+
+    @measurer
+    def measure_channel_3(self):
+        return self._measure_channel(3)
+
+    @measurer
+    def measure_channel_4(self):
+        return self._measure_channel(4)
