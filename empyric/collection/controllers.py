30639400ab3f77b16b738d31b387e88f4914dfe6
dmerthe <36462234+dmerthe@users.noreply.github.com>
2021-04-02 09:19:58 -0700

    Update controllers.py

--- empyric/collection/controllers.py
+++ empyric/collection/controllers.py
@@ -12,7 +12,8 @@ class OmegaCN7500(Instrument):
     supported_adapters = (
         (Modbus, {'slave_mode': 'rtu',
                   'baud_rate': 38400,
-                  'parity': 'N'}),
+                  'parity': 'N',
+                  'delay': 0.2}),
     )
 
     knobs = (
