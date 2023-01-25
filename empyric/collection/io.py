fad191f40144d7c2622a1d058852df3331ec74e5
dmerthe <36462234+dmerthe@users.noreply.github.com>
2023-01-24 20:00:28 -0800

    Fixed ED-560 issue that caused desyncing of channel reads

--- empyric/collection/io.py
+++ empyric/collection/io.py
@@ -1,3 +1,5 @@
+import re
+
 from empyric.collection.instrument import Instrument, setter, getter, measurer
 from empyric.adapters import Socket
 from empyric.tools import recast
@@ -6,8 +8,8 @@ class BrainboxesED560(Instrument):
     """
     Brainboxes 4 channel analog output (0-10 V / 0-20mA) gateway.
 
-    For socket communication, the default port is 9500. If the IP address is unknown, you can use the Boost.IO Driver
-    software from Brainboxes to find it.
+    For socket communication, the default port is 9500. If the IP address is
+    unknown, you can use the Boost.IO Driver software to find it.
 
     ASCII Protocol must be used.
     """
@@ -25,47 +27,55 @@ class BrainboxesED560(Instrument):
 
     @setter
     def set_analog_out0(self, value):
-        self.write('#010%f' % float(value))
+        self.query('#010%f' % float(value), validator=self._set_validator)
 
     @getter
     def get_analog_out0(self):
-        response = self.query('$0160')
+        response = self.query('$0160', validator=self._get_validator)
         return recast(response[3:])
 
     @setter
     def set_analog_out1(self, value):
-        self.write('#011%f' % float(value))
+        self.query('#011%f' % float(value), validator=self._set_validator)
 
     @getter
     def get_analog_out1(self):
-        response = self.query('$0161')
+        response = self.query('$0161', validator=self._get_validator)
         return recast(response[3:])
 
     @setter
     def set_analog_out2(self, value):
-        self.write('#012%f' % float(value))
+        self.query('#012%f' % float(value), validator=self._set_validator)
 
     @getter
     def get_analog_out2(self):
-        response = self.query('$0162')
+        response = self.query('$0162', validator=self._get_validator)
         return recast(response[3:])
 
     @setter
     def set_analog_out3(self, value):
-        self.write('#013%f' % float(value))
+        self.query('#013%f' % float(value), validator=self._set_validator)
 
     @getter
     def get_analog_out3(self):
-        response = self.query('$0163')
+        response = self.query('$0163', validator=self._get_validator)
         return recast(response[3:])
+
+    @staticmethod
+    def _get_validator(response):
+        return re.match('!01\+\d\d\.\d\d\d', response)
+
+    @staticmethod
+    def _set_validator(response):
+        return response.strip() == '>'
 
 
 class BrainboxesED549(Instrument):
     """
     Brainboxes 4 channel analog input (0-10 V / 0-20mA) gateway.
 
-    For socket communication, the default port is 9500. If the IP address is unknown, you can use the Boost.IO Driver
-    software from Brainboxes to find it.
+    For socket communication, the default port is 9500. If the IP address is
+    unknown, you can use the Boost.IO Driver software to find it.
 
     ASCII Protocol must be used.
     """
@@ -83,20 +93,24 @@ class BrainboxesED549(Instrument):
 
     @measurer
     def measure_analog_in0(self):
-        response = self.query('#010')
+        response = self.query('#010', validator=self._validator)
         return recast(response[1:])
 
     @measurer
     def measure_analog_in1(self):
-        response = self.query('#011')
+        response = self.query('#011', validator=self._validator)
         return recast(response[1:])
 
     @measurer
     def measure_analog_in2(self):
-        response = self.query('#012')
+        response = self.query('#012', validator=self._validator)
         return recast(response[1:])
 
     @measurer
     def measure_analog_in3(self):
-        response = self.query('#013')
+        response = self.query('#013', validator=self._validator)
         return recast(response[1:])
+
+    @staticmethod
+    def _validator(response):
+        return re.match('>\+\d\d\.\d\d\d', response)
