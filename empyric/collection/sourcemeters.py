7c9f3fbe7ae278315df1f18d5da5e6970bbe4e0a
Jake Nixon <jnixon@avalanche.energy>
2023-05-31 16:56:21 -0700

    re-add unions

--- empyric/collection/sourcemeters.py
+++ empyric/collection/sourcemeters.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import re
+from typing import Union
 
 import numpy as np
 import pandas as pd
@@ -338,7 +339,7 @@ class Keithley2400(Instrument):
         return self.adapter.delay
 
     @setter
-    def set_fast_voltages(self, voltages: [Array, String]):
+    def set_fast_voltages(self, voltages: Union[Array, String]):
         # import fast voltages, if specified as a path
         if type(voltages) == str:
             is_csv = ".csv" in voltages.lower()
--- empyric/variables.py
+++ empyric/variables.py
@@ -333,7 +333,7 @@ class Remote(Variable):
     def __init__(
         self,
         server: str,
-        alias: [int, str],
+        alias: Union[int, str],
         protocol: str = None,
         settable: bool = False,  # needed for modbus protocol
     ):
